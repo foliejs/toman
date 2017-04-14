6cc21e8650a6717da88cbbbf9b22ce06e1ea76f0
sunnode <m18205253820@163.com>
2017-04-15 07:35:28 +0800

    feature:initization

---
+++ caporal.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const prog = require('caporal');
+prog
+  .version('1.0.0')
+  // you specify arguments using .argument()
+  // 'app' is required, 'env' is optional
+  .command('deploy', 'Deploy an application')
+  .argument('<app>', 'App to deploy', /^myapp|their-app$/)
+  .argument('[env]', 'Environment to deploy on', /^dev|staging|production$/, 'local')
+  // you specify options using .option()
+  // if --tail is passed, its value is required
+  .option('--tail <lines>', 'Tail <lines> lines of logs after deploy', prog.INT)
+  .action(function(args, options, logger) {
+    console.log(args)
+    console.log(options)
+    console.log(logger)
+    // args and options are objects
+    // args = {"app": "myapp", "env": "production"}
+    // options = {"tail" : 100}
+  });
+
+prog.parse(process.argv);
