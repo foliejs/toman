f91b5048c76b19728079b57607118fcee878320d
sunnode <m18205253820@163.com>
2017-04-15 21:28:13 +0800

    chore:init format

--- caporal.js
+++ caporal.js
@@ -6,13 +6,26 @@ const Version = require('./package.json').version
 
 prog
   .version(Version)
-  .help(`Nice Generate !!`)
-  .command('generate', 'Order a pizza')
-  .help(`自定制生成项目的类型`)
+  .help(`
+#    _____               _ 
+#   |_   _|  ___   ___  | |
+#     | |   / _ \ / _ \ | |
+#     |_|   \___/ \___/ |_|
+#                             
+  `)
+  .command('generate', 'generate framework')
+  .help(`alias g`)
   .alias('g')
-  .argument('<project>', '项目框架', ['koa1', 'koa2', 'sails', 'express', 'restify', 'strongLoop'])
+  .argument('<project>', '项目框架', (opt) => {
+    let supportWork = ['koa1', 'koa2', 'sails', 'express', 'restify', 'strongLoop']
+    if (supportWork.includes(opt) === false) {
+      throw new Error('暂不支持该框架')
+    }
+    return opt.toUpperCase()
+  })
   .action((args, options, logger) => {
-    logger.log('I\'m cooking a pizza !')
+    logger.info('arguments: %j', args)
+    logger.info('options: %j', options)
     console.log(`您已选择框架${options},请耐心等待`)
   })
   .complete((args, options, logger) => {
